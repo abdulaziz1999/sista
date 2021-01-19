86ccabbb739136867aa93edd33aabad388075b98
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-19 21:34:09 +0700

    module home

--- application/modules/home/views/home.php
+++ application/modules/home/views/home.php
@@ -1,51 +1,86 @@
-<div class="breadcrumbs ace-save-state" id="breadcrumbs">
-						<ul class="breadcrumb">
-							<li>
-								<i class="ace-icon fa fa-home home-icon"></i>
-								<a href="<?= base_url('admin'); ?>">Dashboard</a>
-							</li>
-							<li class="active">Log Whatsapp</li>
-						</ul>
-	</div>
-        <!-- Main content -->
-        <section class='content'>
-          <div class='row'>
-            <div class='col-xs-12'>
-              <div class='box'>
-                <div class='box-header'>
-                  
-				<h3 class='box-title'>Log Whatsapp<br><br>
-				</h3>
-                </div><!-- /.box-header -->
-        
-        <div class='box-body'>
-        <div class="table-header">
-							Log
-						</div>
-        <div class="table-responsive">
-          <table class="table table-bordered table-striped table-sm" id="table">
-              <thead>
-              <tr> 
-                  <th>No</th>
-                  <th>Pesan</th>
-                  <th>Nomor Handphone</th>
-                  <th>Status</th>
-                  <th>Time</th>
-              </tr>
-              </thead>
-          </table>
+<div class="main-content">
+    <!-- Header -->
+    <div class="header bg-gradient-primary pt-5 pb-7">
+      <div class="container">
+        <div class="header-body">
+          <div class="row align-items-center">
+            <div class="col-lg-12">
+              <div class="pr-5">
+                <h2 class="display-4 text-white font-weight-bold">Sistem Informasi Seminar Tugas Akhir</h2>
+                <p class="text-white mt-4">SISTA adalah Sistem Informasi Seminar Tugas Akhir yang mengintegrasikan administrasi proses seminar tugas akhir. 
+                  Proses pengelolaan seminar tugas akhir dalam SISTA meliputi administrasi proses pengajuan dan pendaftaran pemakalah seminar, peserta seminar, dan penjadwalan seminar. 
+                  SISTA dapat digunakan oleh dua jenis pengguna yaitu administrator dan mahasiswa baik sebagai pemakalah maupun sebagai peserta seminar.</p>
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <div class="row pt-5">
+                <div class="col-md-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-red text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Components</h5>
+                      <p>Argon comes with over 70 handcrafted components.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-blue text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Components</h5>
+                      <p>Argon comes with over 70 handcrafted components.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-yellow text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Components</h5>
+                      <p>Argon comes with over 70 handcrafted components.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <div class="icon icon-shape bg-gradient-green text-white rounded-circle shadow mb-4">
+                        <i class="ni ni-active-40"></i>
+                      </div>
+                      <h5 class="h3">Components</h5>
+                      <p>Argon comes with over 70 handcrafted components.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <script src="<?php echo base_url('assets/js/jquery-1.11.2.min.js') ?>"></script>
-        <script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
-        <script src="<?php echo base_url('assets/datatables/dataTables.bootstrap.js') ?>"></script>
-        <script type="text/javascript">
-            $(document).ready(function () {
-                $("#mytable").dataTable();
-            });
-        </script>
-                    </div><!-- /.box-body -->
-                    
-              </div><!-- /.box -->
-            </div><!-- /.col -->
-          </div><!-- /.row -->
-        </section><!-- /.content -->
+      </div>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <section class="py-6 pb-9 bg-default">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+          <div class="col-md-8">
+            <h2 class="display-3 text-white">A complete HTML solution</h3>
+              <p class="lead text-white">
+                Argon is a completly new product built on our newest re-built from scratch framework structure that is meant to make our products more intuitive,
+                more adaptive and, needless to say, so much easier to customize. Let Argon amaze you with its cool features and build tools and get your project to a whole new level.
+              </p>
+          </div>
+        </div>
+      </div>
+    </section>
+   
+  </div>
