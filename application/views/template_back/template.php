09ff200a45d18212134e1207856a4ca07058594c
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-19 23:15:40 +0700

    template backend modified

--- application/views/template_back/template.php
+++ application/views/template_back/template.php
@@ -40,6 +40,9 @@
   <link rel="stylesheet" href="<?= base_url()?>assets/vendor/nucleo/css/nucleo.css" type="text/css">
   <link rel="stylesheet" href="<?= base_url()?>assets/vendor/%40fortawesome/fontawesome-free/css/all.min.css" type="text/css">
   <!-- Page plugins -->
+  <link rel="stylesheet" href="<?= base_url()?>assets/vendor/datatables.net-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>assets/vendor/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url()?>assets/vendor/datatables.net-select-bs4/css/select.bootstrap4.min.css">
   <!-- Argon CSS -->
   <link rel="stylesheet" href="<?= base_url()?>assets/css/argon.min9f1e.css?v=1.1.0" type="text/css">
 
@@ -71,172 +74,55 @@
           <!-- Nav items -->
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link active" href="#navbar-dashboards" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-dashboards">
+              <a class="nav-link active" href="<?= base_url('dashboard')?>" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-dashboards">
                 <i class="ni ni-shop text-primary"></i>
                 <span class="nav-link-text">Dashboards</span>
               </a>
-              <div class="collapse show" id="navbar-dashboards">
-                <ul class="nav nav-sm flex-column">
-                  <li class="nav-item">
-                    <a href="dashboard.html" class="nav-link">Dashboard</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="alternative.html" class="nav-link">Alternative</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#navbar-examples" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-examples">
-                <i class="ni ni-ungroup text-orange"></i>
-                <span class="nav-link-text">Examples</span>
-              </a>
-              <div class="collapse" id="navbar-examples">
-                <ul class="nav nav-sm flex-column">
-                  <li class="nav-item">
-                    <a href="../examples/pricing.html" class="nav-link">Pricing</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/login.html" class="nav-link">Login</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/register.html" class="nav-link">Register</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/lock.html" class="nav-link">Lock</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/timeline.html" class="nav-link">Timeline</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../examples/profile.html" class="nav-link">Profile</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#navbar-components" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-components">
-                <i class="ni ni-ui-04 text-info"></i>
-                <span class="nav-link-text">Components</span>
-              </a>
-              <div class="collapse" id="navbar-components">
-                <ul class="nav nav-sm flex-column">
-                  <li class="nav-item">
-                    <a href="../components/buttons.html" class="nav-link">Buttons</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../components/cards.html" class="nav-link">Cards</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../components/grid.html" class="nav-link">Grid</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../components/notifications.html" class="nav-link">Notifications</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../components/icons.html" class="nav-link">Icons</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../components/typography.html" class="nav-link">Typography</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="#navbar-multilevel" class="nav-link" data-toggle="collapse" role="button" aria-expanded="true" aria-controls="navbar-multilevel">Multi level</a>
-                    <div class="collapse show" id="navbar-multilevel" style="">
-                      <ul class="nav nav-sm flex-column">
-                        <li class="nav-item">
-                          <a href="#!" class="nav-link ">Third level menu</a>
-                        </li>
-                        <li class="nav-item">
-                          <a href="#!" class="nav-link ">Just another link</a>
-                        </li>
-                        <li class="nav-item">
-                          <a href="#!" class="nav-link ">One last link</a>
-                        </li>
-                      </ul>
-                    </div>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#navbar-forms" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-forms">
-                <i class="ni ni-single-copy-04 text-pink"></i>
-                <span class="nav-link-text">Forms</span>
-              </a>
-              <div class="collapse" id="navbar-forms">
-                <ul class="nav nav-sm flex-column">
-                  <li class="nav-item">
-                    <a href="../forms/elements.html" class="nav-link">Elements</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../forms/components.html" class="nav-link">Components</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../forms/validation.html" class="nav-link">Validation</a>
-                  </li>
-                </ul>
-              </div>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#navbar-tables" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-tables">
                 <i class="ni ni-align-left-2 text-default"></i>
-                <span class="nav-link-text">Tables</span>
+                <span class="nav-link-text">Master Data</span>
               </a>
               <div class="collapse" id="navbar-tables">
                 <ul class="nav nav-sm flex-column">
                   <li class="nav-item">
-                    <a href="../tables/tables.html" class="nav-link">Tables</a>
+                    <a href="<?= base_url('dosen')?>" class="nav-link">Dosen</a>
                   </li>
                   <li class="nav-item">
-                    <a href="../tables/sortable.html" class="nav-link">Sortable</a>
+                    <a href="<?= base_url('mahasiswa')?>" class="nav-link">Mahasiswa</a>
                   </li>
                   <li class="nav-item">
-                    <a href="../tables/datatables.html" class="nav-link">Datatables</a>
+                    <a href="<?= base_url('prodi')?>" class="nav-link">Prodi</a>
                   </li>
                 </ul>
               </div>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#navbar-maps" data-toggle="collapse" role="button" aria-expanded="false" aria-controls="navbar-maps">
-                <i class="ni ni-map-big text-primary"></i>
-                <span class="nav-link-text">Maps</span>
-              </a>
-              <div class="collapse" id="navbar-maps">
-                <ul class="nav nav-sm flex-column">
-                  <li class="nav-item">
-                    <a href="../maps/google.html" class="nav-link">Google</a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="../maps/vector.html" class="nav-link">Vector</a>
-                  </li>
-                </ul>
-              </div>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../widgets.html">
+              <a class="nav-link" href="<?= base_url('df_seminar')?>">
                 <i class="ni ni-archive-2 text-green"></i>
-                <span class="nav-link-text">Widgets</span>
+                <span class="nav-link-text">Daftar Seminar</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../charts.html">
-                <i class="ni ni-chart-pie-35 text-info"></i>
-                <span class="nav-link-text">Charts</span>
+              <a class="nav-link" href="<?= base_url('user')?>">
+                <i class="ni ni-single-02 text-info"></i>
+                <span class="nav-link-text">User</span>
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="../calendar.html">
-                <i class="ni ni-calendar-grid-58 text-red"></i>
-                <span class="nav-link-text">Calendar</span>
+              <a class="nav-link" href="<?= base_url('login/logout')?>">
+                <i class="ni ni-user-run text-red"></i>
+                <span class="nav-link-text">Keluar</span>
               </a>
             </li>
           </ul>
           <!-- Divider -->
-          <hr class="my-3">
+          <!-- <hr class="my-3"> -->
           <!-- Heading -->
-          <h6 class="navbar-heading p-0 text-muted">Documentation</h6>
+          <!-- <h6 class="navbar-heading p-0 text-muted">Documentation</h6> -->
           <!-- Navigation -->
-          <ul class="navbar-nav mb-md-3">
+          <!-- <ul class="navbar-nav mb-md-3">
             <li class="nav-item">
               <a class="nav-link" href="<?= base_url()?>docs/getting-started/overview.html" target="_blank">
                 <i class="ni ni-spaceship"></i>
@@ -261,7 +147,7 @@
                 <span class="nav-link-text">Plugins</span>
               </a>
             </li>
-          </ul>
+          </ul> -->
         </div>
       </div>
     </div>
@@ -518,6 +404,14 @@
   <!-- Optional JS -->
   <script src="<?= base_url()?>assets/vendor/chart.js/dist/Chart.min.js"></script>
   <script src="<?= base_url()?>assets/vendor/chart.js/dist/Chart.extension.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net/js/jquery.dataTables.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-buttons/js/buttons.html5.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-buttons/js/buttons.flash.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-buttons/js/buttons.print.min.js"></script>
+  <script src="<?= base_url()?>assets/vendor/datatables.net-select/js/dataTables.select.min.js"></script>
   <!-- Argon JS -->
   <script src="<?= base_url()?>assets/js/argon.min9f1e.js?v=1.1.0"></script>
   <!-- Demo JS - remove this in your project -->
