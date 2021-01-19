95ccbc3832856646ac60703f57ed29810f8516da
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-19 23:12:25 +0700

    module login change

--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -17,12 +17,45 @@ class Login extends CI_Controller
 		
     }
 
+    // function cekLogin(){
+    //     $u = $this->input->post('username');
+    //     $p = $this->input->post('password');
+
+    //     $cek = $this->Login_model->cek_login($u,$p);
+    //     if ($cek){
+    //         foreach ($cek as $row) {
+    //             $this->session->set_userdata('id_pengguna',$row->id_pengguna);
+	// 			$this->session->set_userdata('username',$row->username);
+	// 			$this->session->set_userdata('nama',$row->nama);
+	// 			$this->session->set_userdata('email',$row->email);
+	// 			$this->session->set_userdata('ttl',$row->ttl);
+    //             $this->session->set_userdata('level',$row->level);
+                
+    //         }
+    //         if ($this->session->userdata('level') == "admin") {
+    //             redirect('admin');
+    //         }elseif ($this->session->userdata('level') == "cs") {
+    //             redirect('cs');
+    //         }elseif ($this->session->userdata('level') == "staff_humas") {
+    //             redirect('sh');
+    //         }
+    //     } else{
+    //         //untuk menendcode kata
+    //         redirect('login?e='.base64_encode('Username dan Password Tidak Sesuai, Coba Lagi'));
+    //     } 
+		
+    // }
+
     public function index()
     {
-		$data['title']		= "Log Whatsapp Center";
+		$data['title']		= "Login SISTA";
         $this->template->load('template_front/template_login','login',$data);
-        // $this->load->view('template_back/template',$data);
-	}
+    }
+    
+    function logout(){
+        $this->session->sess_destroy();
+        redirect('login');
+    }
 
 
 }
