d78dc97f4b44c51d711ac027e0a45c0b9293e491
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-21 21:44:28 +0700

    module prodi

--- application/modules/prodi/controllers/Prodi.php
+++ application/modules/prodi/controllers/Prodi.php
@@ -21,7 +21,47 @@ class Prodi extends CI_Controller
     {
 		$data['title']		= "Prodi";
         $this->template->load('template_back/template','prodi',$data);
-	}
+    }
+    
+    function create(){
+        $data = [
+            'nim'           => $this->input->post('nim'),
+            'nama'          => $this->input->post('nama'),
+            'prodi_id'      => $this->input->post('prodi_id'),
+            'tgl_seminar'   => $this->input->post('tgl_seminar'),
+            'jam_seminar'   => $this->input->post('jam_seminar'),
+            'ruang'         => $this->input->post('ruang'),
+            'judul_ta'      => $this->input->post('judul_ta'),
+            'seminar_id'    => $this->input->post('seminar_id'),
+            'pembimbing'    => $this->input->post('pembimbing'),
+            'penguji1'      => $this->input->post('penguji1'),
+            'penguji2'      => $this->input->post('penguji2'),
+        ];
+
+        $this->db->insert('tb_daftar_seminar',$data);
+    }
+
+    function update($id){
+        $data = [
+            'nim'           => $this->input->post('nim'),
+            'nama'          => $this->input->post('nama'),
+            'prodi_id'      => $this->input->post('prodi_id'),
+            'tgl_seminar'   => $this->input->post('tgl_seminar'),
+            'jam_seminar'   => $this->input->post('jam_seminar'),
+            'ruang'         => $this->input->post('ruang'),
+            'judul_ta'      => $this->input->post('judul_ta'),
+            'seminar_id'    => $this->input->post('seminar_id'),
+            'pembimbing'    => $this->input->post('pembimbing'),
+            'penguji1'      => $this->input->post('penguji1'),
+            'penguji2'      => $this->input->post('penguji2'),
+        ];
+
+        $this->db->update('tb_daftar_seminar',$data,['id_df_seminar' => $id]);
+    }
+
+    function delete($id){
+        $this->db->delete('tb_daftar_seminar',$data,['id_df_seminar' => $id]);
+    }
 
 
 }
