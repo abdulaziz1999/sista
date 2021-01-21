d8b660a8a30dcb81abf3f212470fbc663afc4a2b
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-21 21:41:06 +0700

    module view jadwal

--- application/modules/jadwal/views/jadwal.php
+++ application/modules/jadwal/views/jadwal.php
@@ -7,10 +7,10 @@
             <div class="col-lg-12">
               <div class="">
                   <div class="row align-items-center py-4">
-                    <div class="col-lg-6 col-7">
+                    <div class="col-lg-6 col-sm-10">
                       <h2 class="display-4 text-white font-weight-bold">Sistem Informasi Seminar Tugas Akhir</h2>
                     </div>
-                    <div class="col-lg-6 col-5 text-right">
+                    <div class="col-lg-6 col-sm-2 text-right">
                       <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
                           <li class="breadcrumb-item"><a href="<?= base_url()?>"><i class="fas fa-home"></i> Home</a></li>
@@ -25,156 +25,31 @@
                       <h3 class="mb-0">Jadwal Seminar Tugas Akhir</h3>
                     </div>
                     <div class="table-responsive py-4">
-                      <table class="table table-flush" id="datatable-basic">
-                        <thead class="thead-light">
+                    <table class="table table-flush" id="datatable-basic">
+                      <thead class="thead-light">
+                        <tr>
+                          <th>No</th>
+                          <th>NIM</th>
+                          <th>Mahasiswa/i</th>
+                          <th>Seminar</th>
+                          <th>Waktu</th>
+                          <th>Ruangan</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <?php $no=1; foreach($daftar_seminar as $row):?>
                           <tr>
-                            <th>No</th>
-                            <th>NIM</th>
-                            <th>Mahasiswa</th>
-                            <th>Seminar</th>
-                            <th>Waktu</th>
-                            <th>Ruangan</th>
-                            <th>*</th>
+                            <td><?= $no++?></td>
+                            <td><?= $row->nim?></td>
+                            <td><a href="<?= site_url('jadwal/detail/'.$row->id_df_seminar)?>"><?= $row->nama?></a></td>
+                            <td><?= $row->nama_seminar?></td>
+                            <td><?= $row->jam_seminar."  ".date('d-m-Y',strtotime($row->tgl_seminar))?></td>
+                            <td><?= $row->ruang?></td>
                           </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>Tiger Nixon</td>
-                            <td>System Architect</td>
-                            <td>Edinburgh</td>
-                            <td>61</td>
-                            <td>2011/04/25</td>
-                            <td>$320,800</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Garrett Winters</td>
-                            <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>63</td>
-                            <td>2011/07/25</td>
-                            <td>$170,750</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Ashton Cox</td>
-                            <td>Junior Technical Author</td>
-                            <td>San Francisco</td>
-                            <td>66</td>
-                            <td>2009/01/12</td>
-                            <td>$86,000</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Cedric Kelly</td>
-                            <td>Senior Javascript Developer</td>
-                            <td>Edinburgh</td>
-                            <td>22</td>
-                            <td>2012/03/29</td>
-                            <td>$433,060</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Airi Satou</td>
-                            <td>Accountant</td>
-                            <td>Tokyo</td>
-                            <td>33</td>
-                            <td>2008/11/28</td>
-                            <td>$162,700</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Brielle Williamson</td>
-                            <td>Integration Specialist</td>
-                            <td>New York</td>
-                            <td>61</td>
-                            <td>2012/12/02</td>
-                            <td>$372,000</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Herrod Chandler</td>
-                            <td>Sales Assistant</td>
-                            <td>San Francisco</td>
-                            <td>59</td>
-                            <td>2012/08/06</td>
-                            <td>$137,500</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Rhona Davidson</td>
-                            <td>Integration Specialist</td>
-                            <td>Tokyo</td>
-                            <td>55</td>
-                            <td>2010/10/14</td>
-                            <td>$327,900</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Colleen Hurst</td>
-                            <td>Javascript Developer</td>
-                            <td>San Francisco</td>
-                            <td>39</td>
-                            <td>2009/09/15</td>
-                            <td>$205,500</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Sonya Frost</td>
-                            <td>Software Engineer</td>
-                            <td>Edinburgh</td>
-                            <td>23</td>
-                            <td>2008/12/13</td>
-                            <td>$103,600</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Jena Gaines</td>
-                            <td>Office Manager</td>
-                            <td>London</td>
-                            <td>30</td>
-                            <td>2008/12/19</td>
-                            <td>$90,560</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Quinn Flynn</td>
-                            <td>Support Lead</td>
-                            <td>Edinburgh</td>
-                            <td>22</td>
-                            <td>2013/03/03</td>
-                            <td>$342,000</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Charde Marshall</td>
-                            <td>Regional Director</td>
-                            <td>San Francisco</td>
-                            <td>36</td>
-                            <td>2008/10/16</td>
-                            <td>$470,600</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Haley Kennedy</td>
-                            <td>Senior Marketing Designer</td>
-                            <td>London</td>
-                            <td>43</td>
-                            <td>2012/12/18</td>
-                            <td>$313,500</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                          <tr>
-                            <td>Tatyana Fitzpatrick</td>
-                            <td>Regional Director</td>
-                            <td>London</td>
-                            <td>19</td>
-                            <td>2010/03/17</td>
-                            <td>$385,750</td>
-                            <td><button type="button" class="btn btn-success btn-sm"><i class="fas fa-eye"></i> Detail</button></td>
-                          </tr>
-                        </tbody>
-                      </table>
+                          
+                        <?php endforeach;?>
+                      </tbody>
+                    </table>
                     </div>
                   </div>
               </div>
