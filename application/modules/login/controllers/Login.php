990fc2ae6367910cefa6594ae648ecd84da7c575
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-21 21:41:54 +0700

    module login

--- application/modules/login/controllers/Login.php
+++ application/modules/login/controllers/Login.php
@@ -17,34 +17,50 @@ class Login extends CI_Controller
 		
     }
 
-    // function cekLogin(){
-    //     $u = $this->input->post('username');
-    //     $p = $this->input->post('password');
+    function cekLogin(){
+        $u = $this->input->post('email');
+        $p = $this->input->post('password');
 
-    //     $cek = $this->Login_model->cek_login($u,$p);
-    //     if ($cek){
-    //         foreach ($cek as $row) {
-    //             $this->session->set_userdata('id_pengguna',$row->id_pengguna);
-	// 			$this->session->set_userdata('username',$row->username);
-	// 			$this->session->set_userdata('nama',$row->nama);
-	// 			$this->session->set_userdata('email',$row->email);
-	// 			$this->session->set_userdata('ttl',$row->ttl);
-    //             $this->session->set_userdata('level',$row->level);
+        $cek = $this->cek_login($u,$p);
+        if ($cek){
+            foreach ($cek as $row) {
+                $this->session->set_userdata('id_user',$row->id_user);
+				$this->session->set_userdata('nama',$row->nama);
+				$this->session->set_userdata('email',$row->email);
+                $this->session->set_userdata('level',$row->level);
                 
-    //         }
-    //         if ($this->session->userdata('level') == "admin") {
-    //             redirect('admin');
-    //         }elseif ($this->session->userdata('level') == "cs") {
-    //             redirect('cs');
-    //         }elseif ($this->session->userdata('level') == "staff_humas") {
-    //             redirect('sh');
-    //         }
-    //     } else{
-    //         //untuk menendcode kata
-    //         redirect('login?e='.base64_encode('Username dan Password Tidak Sesuai, Coba Lagi'));
-    //     } 
+            }
+            if ($this->session->userdata('level') == "admin") {
+                redirect('dashboard');
+            }elseif ($this->session->userdata('level') == "mahasiswa") {
+                redirect('home');
+            }elseif ($this->session->userdata('level') == "dosen") {
+                redirect('jadwal');
+            }
+        } else{
+            //untuk menendcode kata
+            redirect('login?e='.base64_encode('Username dan Password Tidak Sesuai, Coba Lagi'));
+        } 
 		
-    // }
+    }
+
+    function cek_login($u,$p){
+        $pwd = md5($p);
+        $this->db->select('*');
+        $this->db->from('tb_user');
+        $this->db->where('email',$u);
+        $this->db->where('password',$pwd);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+
+        if ($query->num_rows()==1) {
+        	return $query->result();
+        }else{
+        	return false;
+        }
+        
+    }
 
     public function index()
     {
