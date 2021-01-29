167ddf9d61bbcdd9d2f36a33a3901d4d8588a067
abdulaziz1999 <azizmentor96@gmail.com>
2021-01-29 07:03:50 +0700

    module register

--- application/modules/register/controllers/Register.php
+++ application/modules/register/controllers/Register.php
@@ -21,7 +21,33 @@ class Register extends CI_Controller
     {
 		$data['title']		= "Registrasi";
         $this->template->load('template_front/template_login','register',$data);
-	}
+    }
+    
+    function create(){
+        $data = [
+            'nama_mhs'  => $this->input->post('nama_mhs'),
+            'nim'       => $this->input->post('nim'),
+            'tmp_lahir' => $this->input->post('tmp_lahir'),
+            'tgl_lahir' => $this->input->post('tgl_lahir'),
+            'jk'        => $this->input->post('jk'),
+            'agama'     => $this->input->post('agama'),
+            'alamat'    => $this->input->post('alamat'),
+            'prodi_id'  => $this->input->post('prodi_id'),
+        ];
+
+        $this->db->insert('tb_mahasiswa',$data);
+        $id = $this->db->insert_id();
+        $data = [
+            'nama'          => $this->input->post('nama'),
+            'email'         => $this->input->post('email'),
+            'level'         => 'mahasiswa',
+            'password'      => md5($this->input->post('password')),
+            'user_id'       => $id
+        ];
+
+        $this->db->insert('tb_user',$data);
+        redirect($_SERVER['HTTP_REFERER']);
+    }
 
 
 }
